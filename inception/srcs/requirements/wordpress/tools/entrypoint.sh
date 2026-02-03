1365879d4b46853f6bedd77ca6682cf46ffe2fcf
Hong-CHP <93changhong@gmail.com>
2026-02-03 22:54:09 +0100

    first_try_wp_after_mariadb_success

--- inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,13 +1,48 @@
 #!/bin/bash
 
-set -eu
+# set -u
 
-if [ -f "/etc/php/8.2/fpm/pool.d/www.conf" ]; then
-      sed -i "s|listen = /run/php/php8.2-fpm.sock|listen = 9000|g" /etc/php/8.2/fpm/pool.d/www.conf
-fi
+# if [ -f "/etc/php/8.2/fpm/pool.d/www.conf" ]; then
+#       sed -i "s|listen = /run/php/php8.2-fpm.sock|listen = 9000|g" /etc/php/8.2/fpm/pool.d/www.conf
+# fi
 
-if [ -z "$(ls -A /var/www/html)" ]; then
+# if [ -z "$(ls -A /var/www/html)" ]; then
+#     cp -r /usr/src/wordpress/* /var/www/html/
+# fi
+
+# if [ ! -f "/var/www/html/wp-config.php" ]; then
+# 	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+# 	sed -i "s/database_name_here/${MYSQL_DATABASE}/" /var/www/html/wp-config.php
+# 	sed -i "s/username_here/${MYSQL_USER}/" /var/www/html/wp-config.php
+# 	sed -i "s/password_here/$(cat ${MYSQL_PASSWORD_FILE})/" /var/www/html/wp-config.php
+# 	sed -i "s/localhost/${MYSQL_HOST}/" /var/www/html/wp-config.php
+# fi
+
+# exec "$@"
+
+set -u
+
+echo "=== WordPress Initialization ==="
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+echo "Waiting for MariaDB.."
+while ! nc -z "${MYSQL_HOST}" 3306; do
+    sleep 2
+done
+
+echo "MariaDB is ready!"
+sleep 5
+
+MYSQL_PASSWORD=$(cat "/run/secrets/db_password")
+
+echo "Checking database connection with wp-cli.."
+sleep 10
+
+if [ ! -f "/var/www/html/wp-settings.php" ]; then
     cp -r /usr/src/wordpress/* /var/www/html/
+	chown -R www-data:www-data /var/www/html
 fi
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
@@ -15,8 +50,41 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 
 	sed -i "s/database_name_here/${MYSQL_DATABASE}/" /var/www/html/wp-config.php
 	sed -i "s/username_here/${MYSQL_USER}/" /var/www/html/wp-config.php
-	sed -i "s/password_here/$(cat ${MYSQL_PASSWORD_FILE})/" /var/www/html/wp-config.php
+	sed -i "s/password_here/${MYSQL_PASSWORD}/" /var/www/html/wp-config.php
 	sed -i "s/localhost/${MYSQL_HOST}/" /var/www/html/wp-config.php
 fi
+
+if ! wp core is-installed --path=/var/www/html --allow-root 2>/dev/null; then
+	ADMIN_USER=$(grep WP_ADMIN_USER /run/secrets/credentials | cut -d '=' -f2)
+	ADMIN_PWD=$(grep WP_ADMIN_PASSWORD /run/secrets/credentials | cut -d '=' -f2)
+	WPU_USR=$(grep WP_USR /run/secrets/credentials | cut -d '=' -f2)
+    WPU_PWD=$(grep WP_PWD /run/secrets/credentials | cut -d '=' -f2)
+    WPU_EMAIL=$(grep WP_EMAIL /run/secrets/credentials | cut -d '=' -f2)
+
+	echo "WordPress database not found. Installing..."
+	wp core install \
+		--path=/var/www/html \
+		--url="${DOMAIN_NAME}" \
+		--title="Inception" \
+		--admin_user="${ADMIN_USER}" \
+		--admin_password="${ADMIN_PWD}" \
+		--admin_email="hporta-c@student.42.fr" \
+		--skip-email \
+		--allow-root
+
+	wp user create \
+		"${WPU_USR}" "${WPU_EMAIL}" \
+		--user_pass="${WPU_PWD}" \
+        --role=author \
+		--path=/var/www/html \
+		--allow-root
+else
+	echo "WordPress already installed."
+fi
+
+mkdir -p /run/php
+chown www-data:www-data /run/php
+
+echo "WordPress launch succesfully!"
 
 exec "$@"
